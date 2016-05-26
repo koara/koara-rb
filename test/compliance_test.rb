e0aa1060530c14bc1d634b8525dfe94d2983f42e
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-05-26 19:01:20 +0200

    make parameterized tests

--- test/compliance_test.rb
+++ test/compliance_test.rb
@@ -2,19 +2,27 @@ require_relative '../lib/parser'
 require_relative '../lib/ast/document'
 require_relative 'html5renderer'
 require "test/unit"
+require 'pathname'
 
 class ComplianceTest < Test::Unit::TestCase
 
-  def test_koara_to_html5
-    kd = File.read('/Users/andy/git/koara/testsuite/input/paragraphs/paragraphs-001-simple.kd')
-    html = File.read('/Users/andy/git/koara/testsuite/output/html5/paragraphs/paragraphs-001-simple.htm')
-   
-    parser = Parser.new
-    document = parser.parse(kd)
-    renderer = Html5Renderer.new
-    document.accept(renderer)
-    assert_equal(html, renderer.output)
+
+  @tests = Dir.glob('/Users/andy/git/koara/testsuite/input/**/*')
+
+
+  @tests.each do |test|
+    define_method("test_#{test}") do
+
+
+      puts "//" + Pathname.new(test).basename
+      #kd = File.read("/Users/andy/git/koara/testsuite/input/paragraphs/#{test}.kd")
+      #html = File.read("/Users/andy/git/koara/testsuite/output/html5/paragraphs/#{test}.htm")
+
+      #parser = Parser.new
+      #document = parser.parse(kd)
+      #renderer = Html5Renderer.new
+      #document.accept(renderer)
+      #assert_equal(html, renderer.output)
+    end
   end
-
-
 end
